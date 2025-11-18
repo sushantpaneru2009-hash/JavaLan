04fafa078cb71172fef8cf7b0f26f5be4e6a4d79
Sushant Paneru <sushantpaneru2009@gmail.com>
2025-11-18 22:07:23 +0545

    Initial sharing of project

---
+++ Week_1.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Week_1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Week_1
+{
+   public static void main(String[] args)
+   {
+       System.out.println("It\'s my first program ");
+    
+    }
+}
