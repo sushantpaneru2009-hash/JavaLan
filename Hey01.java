0b5651e3f4df776e19a2cf200b371f770c521b55
Sushant Paneru <sushantpaneru2009@gmail.com>
2025-11-18 22:09:16 +0545

    Added the first hey Program

---
+++ Hey01.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Hey01 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hey01
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Hey01
+     */
+    public Hey01()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
